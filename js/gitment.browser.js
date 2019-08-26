01596b5a801c6c34d0f0cf697d807ad86a7e65f3
luuman <>
2019-08-27 00:44:38 +0800

    Site updated: 2019-08-27 00:44:37

--- js/gitment.browser.js
+++ js/gitment.browser.js
@@ -3413,7 +3413,7 @@ var Gitment = function () {
       }, options);
 
       this.state.user.isLoggingIn = true;
-      _utils.http.post('https://cors-anywhere.herokuapp.com/https://github.com/login/oauth/access_token', {
+      _utils.http.post('https://gh-oauth.imsun.net', {
         code: code,
         client_id: client_id,
         client_secret: client_secret
